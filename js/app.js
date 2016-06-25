7feb596ba2af0f9b412665dbe643dca90a2350d5
jprittie <Prittie@Jennifers-MacBook-Pro.local>
2016-06-25 11:23:23 -0400

    Project files added

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+
+// 1. When the page loads, display start screen
+$(function(){
+  // Hide game fields
+  $("#board").hide();
+
+  // Add start screen
+  $("body").append('<div class="screen screen-start" id="start"><header> <h1>Tic Tac Toe</h1> <a href="#" class="button">Start game</a> </header></div>');
+});
+
+// When the player clicks start button, the start screen disappears,
+// the board appears and the game begins
+$(".button").click(function(){
+  console.log("Start button clicked");
+  $("#start").hide();
+  $("#board").show();
+
+});
