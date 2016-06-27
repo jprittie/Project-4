bb84a49579e1b329c2de0c8c27ddaa6e9d4d0aed
jprittie <Prittie@Jennifers-MacBook-Pro.local>
2016-06-27 12:53:59 -0400

    Game reset added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
+
+// Add module function
+
+// Initialize variables
 var winningarray = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
 var oharray = [];
 var exarray = [];
 var fillcounter = 0;
 
-// When the page loads, display start screen
+// When the page loads, append start and finish screens so they are available,
+// but only display start screen
 $(function(){
-  // Hide game fields
+  // Board screen already exists, so hide it
+  // Actually, I should just take it out of the html and append & hide it here
   $("#board").hide();
 
   // Add start screen
   $("body").append('<div class="screen screen-start" id="start"><header> <h1>Tic Tac Toe</h1> <a href="#" class="button">Start game</a> </header></div>');
+
+  // Add finish screen
+  $("body").append('<div class="screen screen-win" id="finish"><header><h1>Tic Tac Toe</h1><p class="message"></p><a href="#" class="button newgamebutton">New game</a></header></div>');
+  // Hide finish screen
+  $("#finish").hide();
+
 
     // When the player clicks start button, the start screen disappears,
     // the board appears and the game begins
@@ -17,14 +29,21 @@ $(function(){
       event.preventDefault();
       $("#start").hide();
       $("#player1").addClass("active");
-      // But should I remove board above and append it here for consistency?
+
+      // The rest of this should be in a game reset function,
+      // which I don't need to call here
+      $(".screen-win").removeClass("screen-win-one");
+      $(".screen-win").removeClass("screen-win-two");
+      $(".screen-win").removeClass("screen-win-tie");
+      $(".message").text("");
       $("#board").show();
     });
 
 
 
 // On hover, player-specific svg appears
-    $(".boxes li").hover(function() {
+// Note: hover method takes two functions as parameters
+    $(".boxes li").hover(function () {
         if ($("#player1").hasClass("active")) {
           $(this).css("background-image", "url(./img/o.svg)");
         }
@@ -60,69 +79,107 @@ $(function(){
      included in oharray or exarray. If they are, game is over. If they are not, we keep going
      until there is a draw or a winner.
   */
-var nowinner = true;
+    var nowinner = true;
 
-for (var i=0; i<winningarray.length; i++){
-  var firsttest = winningarray[i][0];
-  var secondtest = winningarray[i][1];
-  var thirdtest = winningarray[i][2];
+    for (var i=0; i<winningarray.length; i++){
+      var firsttest = winningarray[i][0];
+      var secondtest = winningarray[i][1];
+      var thirdtest = winningarray[i][2];
 
-  if (oharray.includes(firsttest) && oharray.includes(secondtest) && oharray.includes(thirdtest)) {
-    console.log("O wins");
-    nowinner = false;
-  } else if (exarray.includes(firsttest) && exarray.includes(secondtest) && exarray.includes(thirdtest)) {
-    console.log("X wins");
-    nowinner = false;
-  } else {
-    nowinner = true;
-    drawTest();
-  }
+      if (oharray.includes(firsttest) && oharray.includes(secondtest) && oharray.includes(thirdtest)) {
+        console.log("O wins");
+        nowinner == false;
+        $("#board").hide();
+        $(".message").text("Winner");
+        $(".screen-win").addClass("screen-win-one");
+        $("#finish").show()
+      } else if (exarray.includes(firsttest) && exarray.includes(secondtest) && exarray.includes(thirdtest)) {
+        console.log("X wins");
+        nowinner == false;
+        $("#board").hide();
+        $(".message").text("Winner");
+        $(".screen-win").addClass("screen-win-two");
+        $("#finish").show();
+      } else {
+        nowinner == true;
+        drawTest();
+      }
 
-}
-
-// How do I test for a draw?
-// If all squares are filled and nowinner == true
-// Cycle through all the boxes
-// if box has class .box-filled-1 or .box-filled-2
-function drawTest(){
-    fillcounter = 0;
-
-        $(".boxes li").each(function(){
-            if ( $(this).hasClass("box-filled-1") || $(this).hasClass("box-filled-2") ) {
-              fillcounter += 1;
-            }
-        });
-
-    console.log("fillcounter is " + fillcounter);
-    if ( (fillcounter == 9) && (nowinner === true) ) {
-      console.log("It's a draw.");
     }
 
-}
+// When new game button is clicked, restart game
+$(".newgamebutton").click(function(){
+
+    // Must clear all filled boxes and svg background images
+    $(".boxes li").removeClass("box-filled-1");
+    $(".boxes li").removeClass("box-filled-2");
+    $(".boxes li").css("background-image", "none");
+    // Clear player arrays and fillcounter
+    oharray = [];
+    exarray = [];
+    fillcounter = 0;
 
 
 
-  // You want to do this before toggling active class
+    $("#finish").hide();
+    // Clear winning screens, tie screen and winner/it's a draw message
+    $(".screen-win").removeClass("screen-win-one");
+    $(".screen-win").removeClass("screen-win-two");
+    $(".screen-win").removeClass("screen-win-tie");
+    $(".message").text("");
+
+    // I don't know why player 2 is still active at this point,
+    // but make it inactive
+    $("#player2").removeClass("active");
+
+
+    $("#board").show();
+
+    // Re-enable hover on boxes
+    // I shouldn't have to write all this out again,
+    // but I'm not sure how to fix it
+    $(".boxes li").hover(function () {
+        if ($("#player1").hasClass("active")) {
+          $(this).css("background-image", "url(./img/o.svg)");
+        }
+        else {
+          $(this).css("background-image", "url(./img/x.svg)");
+
+        }
+    }, function() {
+      $(this).css("background-image", "none");
+    });
+});
+
+
+
+
+
+    // At the end of each turn, toggle the active player
     $("#player1").toggleClass("active");
     $("#player2").toggleClass("active");
+
   });
 
 
 });
 
 
+// This function tests the board to see if there's a draw
+function drawTest(){
+    fillcounter = 0;
+       // Count how many boxes have class of .box-filled-1 or .box-filled-2
+        $(".boxes li").each(function(){
+            if ( $(this).hasClass("box-filled-1") || $(this).hasClass("box-filled-2") ) {
+              fillcounter += 1;
+            }
+        });
+    // If all nine squares are filled and nowinner == true, it's a draw
+    if ( (fillcounter == 9) && (nowinner === true) ) {
+      $(".message").text("It's a draw");
+      $(".screen-win").addClass("screen-win-tie");
+      $("#board").hide();
+      $("#finish").show();
+    }
 
-
-/*
-- for each turn:
-    - on hover over each square, svg appears
-    - player picks a square by clicking
-    - svg is added to that square
-    - blue background is added to that square
-    - then that square isn't available anymore
-    - can I use filter to weed out squares that are taken?
-    - test state of board against all possible solutions
-    - if player wins, indicate that
-    - game is over
-    - else, active class toggles to other player
-*/
+}
